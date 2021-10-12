61d78f139300c5ab95abc914a8ac171aedbb278a
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-10-12 22:31:56 +0000

    Done for the day

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -78,13 +78,92 @@ class Login extends StatelessWidget {
             SizedBox(
               height: 25,
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Color(0xff27B9F1),
-              ),
-              margin: EdgeInsets.only(left: 25, right: 25),
-              height: 65,
+            Column(
+              children: [
+                Container(
+                  child: Center(
+                      child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  )),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xff27B9F1),
+                  ),
+                  margin: EdgeInsets.only(left: 25, right: 25),
+                  height: 65,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "Or",
+                  style: TextStyle(fontSize: 25, color: kblackAsh),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  child: Center(
+                      child: Text(
+                    "Continue with Facebook",
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  )),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xff4267B2),
+                  ),
+                  margin: EdgeInsets.only(left: 25, right: 25),
+                  height: 65,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  child: Center(
+                      child: Text(
+                    "Continue with Google",
+                    style: TextStyle(fontSize: 25, color: Colors.black),
+                  )),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      border: Border.all(color: kblackAsh)),
+                  margin: EdgeInsets.only(left: 25, right: 25),
+                  height: 65,
+                ),
+                SizedBox(height: 25),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          "Don't have an account yet?",
+                          style: TextStyle(fontSize: 25, color: kblackAsh),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "Create Account",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          Text(
+                            "                                                  ",
+                            style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.overline,
+                              decorationThickness: 2,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
             )
           ],
         ),
